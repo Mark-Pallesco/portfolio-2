5436d1542345e7ef2020b5eb8e7d70b6444ee4ca
Mark-Pallesco <angelomark31@gmail.com>
2024-11-27 17:07:04 +0800

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,55 @@
 import Header from "./Components/Header";
-import { useState } from 'react';
+import { useState } from "react";
+import logo from './assets/mrk-logo-white.png';
 
 export default function App() {
-  const [name, setName] = useState('Lorem Lorem');
-  const [inputValue, setInputValue] = useState(''); 
+  const [name, setName] = useState("Lorem Lorem");
+  const [inputValue, setInputValue] = useState("");
 
   const handleChange = (event) => {
-    setInputValue(event.target.value); 
+    setInputValue(event.target.value);
   };
 
   const updateName = () => {
     setName(inputValue);
-    setInputValue(''); 
+    setInputValue("");
   };
 
   return (
     <div className="bg-darkbg">
       <Header />
-      <main className="w-full h-screen flex justify-center items-center bg-grid-gray-500 bg-grid-8" id="info">
-        <section className="text-white gap-4 flex flex-col items-center w-full z-10 ">
-          <h3 className="font-sans text-2xl sm:text-xl">Lorem, ipsum.</h3>
-          <h1 className="font-bold text-5xl md:text-4xl sm:text-3xl">{name}</h1>
-          <h2 className="text-2xl sm:text-xl">Lorem, ipsum dolor.</h2>
-          <button 
-            className="px-8 py-2 bg-transparent text-xl text-white rounded-3xl border border-white hover:bg-white hover:text-darkbg transition duration-300"
-          >
-            Learn More
-          </button>
+
+      <main
+        className="w-full h-screen flex justify-center items-center bg-grid-gray-500 bg-grid-8"
+        id="info"
+      >
+        <section className="text-white gap-4 flex flex-row-reverse items-center w-full z-10 px-10">
+          <div className="text-white w-full gap-4 flex flex-col items-center">
+            <h3 className="font-sans text-2xl sm:text-xl">Lorem, ipsum.</h3>
+            <h1 className="font-bold text-5xl md:text-4xl sm:text-3xl">{name}</h1>
+            <h2 className="text-2xl sm:text-xl">Lorem, ipsum dolor.</h2>
+            <button className="px-8 py-2 bg-transparent text-xl text-white rounded-3xl border border-white hover:bg-white hover:text-darkbg transition duration-300">
+              Learn More
+            </button>
+          </div>
+
+          <div className="text-white w-full gap-4 flex flex-col items-center">
+            <img src={logo} alt="Logo" />
+          </div>
         </section>
       </main>
 
-      <div id="about" >
-          <div className="w-full" >
-                <div className="text-white" >
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi autem obcaecati corporis minus, facilis perferendis suscipit quibusdam sit? Autem, repudiandae?</p>
-                    <h2>Lorem Lorem</h2>
-                </div>
-          </div>
+      {/* About Section */}
+      <div id="about" className="w-full">
+        <div className="text-white px-10">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi autem
+            obcaecati corporis minus, facilis perferendis suscipit quibusdam
+            sit? Autem, repudiandae?
+          </p>
+          <h2 className="text-2xl font-bold mt-4">{name}</h2>
+        </div>
       </div>
-
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-        <div className="absolute w-full text-white z-10 bg-darkbg border-b border-white border-opacity-50" >
+        <div className="absolute  w-full text-white z-10 bg-darkbg border-b border-white border-opacity-10" >
             <div  className="px-10 py-5 relative">
                 <header className="flex justify-between" >
                     <h3 className="text-4xl text-white font-bold" ><a href="#">mrk.dev</a></h3>
