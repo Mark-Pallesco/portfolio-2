c2cb73b1d4e6cef7eca02994468488ff984d1110
Mark-Pallesco <angelomark31@gmail.com>
2024-11-27 16:20:50 +0800

    name

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
         <div className="absolute w-full text-white z-10 bg-darkbg border-b border-white border-opacity-50" >
             <div  className="px-10 py-5 relative">
                 <header className="flex justify-between" >
-                    <h3 className="text-4xl text-white font-bold" ><a href="#">mrkpalle.dev</a></h3>
+                    <h3 className="text-4xl text-white font-bold" ><a href="#">mrk.dev</a></h3>
                     <nav>
                         <ul className="flex gap-4" >
                         <li className="hover:bg-white hover:text-darkbg p-2 rounded-sm "><a href="#home">Home</a></li>
