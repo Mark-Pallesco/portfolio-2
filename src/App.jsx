827b823470170f7fbd3b558a0035a567d24d2beb
Mark Angelo Pallesco <angelomark31@gmail.com>
2025-08-09 16:54:11 +0800

    dasda

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import Banner from "./sections/banner";
 import Services from "./sections/services";
-import Header from "./components/Header"; 
+
 
 export default function App() {
   return (
     <div className="w-full relative">
-      <Header />
       <main id="page">
         <Banner />
         <Services />
