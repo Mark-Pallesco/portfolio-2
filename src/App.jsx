55c742a471bb207730de70eeb793375d2a93e99f
Mark Angelo Pallesco <angelomark31@gmail.com>
2025-08-09 16:36:38 +0800

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
-import Banner from "./sections/Banner.jsx";
-import Services from "./sections/Services.jsx";
+import Banner from "./sections/banner";
+import Services from "./sections/services";
 
 export default function App() {
   return (
